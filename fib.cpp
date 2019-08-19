12aa468ec409fccc7b8d470b42cd57d236aa9017
Jayakumar <jaykumar.r@gmail.com>
2019-08-18 22:18:02 -0700

    first one

---
+++ fib.cpp
@@ -0,0 +1,45 @@
+// this is dummy code to print square root of even numbers less than  n ..
+// where n is parameter.. 
+// purpose is to demonstrate profiler..
+// usage : ./fib <n> <m>
+#include <iostream>
+#include <math.h>
+#include "etprof.h"
+void print_sqrt(int);
+long fibonocci(long);
+using namespace std;
+int main(int argc, char *argv[])
+{
+bench_mark_fun ; //*** This activates profiler for main function ****//
+int n = 20; // default
+if ( argc < 2 ) {
+	cerr << " number parameter missing\n taking default";
+}
+else 
+{
+    n = atoi(argv[1]);
+}
+
+if ( n < 0 ) {
+	cerr << " enter small +ve integer\n";
+	return -2;
+}
+
+for ( int i=1; i <= n ; i++ )
+{
+	bench_mark_block_id(for_loop); //*** This activates profiler for for() loop ****//
+	cout <<  " fibonocci series for num("  << i << ") = " << fibonocci(i) << "##\n";
+}
+
+return 0;
+}
+
+long fibonocci(long  n)
+{
+	bench_mark_fun;    //*** This activates profiler for this function ****//
+	cout<< "fib( "<<n<<" ) - ";
+	if ( n == 0 || n == 1 )
+		return 1;
+	else 
+		return fibonocci(n-1) + fibonocci(n-2) ;
+}
